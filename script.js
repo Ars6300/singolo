6e066210584623ca6fc96948de19bc4b76da6f71
ars6300 <ars6300@live.com>
2020-03-14 14:06:08 +0500

    feat: add dom functionality

---
+++ script.js
@@ -0,0 +1,157 @@
+const MENU = document.querySelector("body > header > nav > ul");
+const MAIN = document.querySelector("body > main");
+const HEADER = document.querySelector("body > header");
+const SLIDER = document.getElementById("#slider");
+const SUBMIT = document.querySelector(
+  "#quote > div > form > input:nth-child(5)"
+);
+const CLOSE_BUTTON = document.getElementById("close-btn");
+const FORM = document.querySelector("#quote > div > form");
+const MESSAGE_BLOCK = document.getElementById("message-block");
+const NAME_INPUT = document.querySelector(
+  "#quote > div > form > input:nth-child(1)"
+);
+const EMAIL_INPUT = document.querySelector(
+  "#quote > div > form > input:nth-child(2)"
+);
+const HORIZONTAL_PHONE = document.getElementById("horizontal");
+const VERTICAL_PHONE = document.getElementById("vertical");
+const LEFT = document.getElementById("left");
+const RIGHT = document.getElementById("right");
+
+const PORTFOLIO = document.querySelector("#portfolio > nav > ul");
+const CONTAINER = document.querySelector("#portfolio > div");
+
+MAIN.style.marginTop = HEADER.offsetHeight + "px";
+
+MENU.addEventListener("click", event => {
+  MENU.querySelectorAll("li > a").forEach(el => el.classList.remove("active"));
+  event.target.classList.add("active");
+});
+
+LEFT.addEventListener("click", () => {
+  if (document.querySelector("#slides1").classList.contains("hidden")) {
+    document.querySelector("#slides1").style.animation =
+      "slideLeftOpen 0.3s linear forwards";
+    document.querySelector("#slides2").style.animation =
+      "slideLeftClose 0.3s linear forwards";
+    setTimeout(function() {
+      document.querySelector("#slides1").classList.remove("hidden");
+      document.querySelector("#slides2").classList.add("hidden");
+      document.querySelector("#slider").style.backgroundColor = "#f06c64";
+      document.querySelector("#slider").style.borderColor = "#ea676b";
+    }, 300);
+  } else {
+    document.querySelector("#slides1").style.animation =
+      "slideLeftClose 0.3s linear forwards";
+    document.querySelector("#slides2").style.animation =
+      "slideLeftOpen 0.3s linear forwards";
+    setTimeout(function() {
+      document.querySelector("#slides1").classList.add("hidden");
+      document.querySelector("#slider").style.backgroundColor = "#648BF0";
+      document.querySelector("#slider").style.borderColor = "#648BF0";
+      document.querySelector("#slides2").classList.remove("hidden");
+    }, 300);
+  }
+});
+
+RIGHT.addEventListener("click", () => {
+  if (document.querySelector("#slides1").classList.contains("hidden")) {
+    document.querySelector("#slides1").style.animation =
+      "slideRightOpen 0.3s linear forwards";
+    document.querySelector("#slides2").style.animation =
+      "slideRightClose 0.3s linear forwards";
+    setTimeout(function() {
+      document.querySelector("#slides1").classList.remove("hidden");
+      document.querySelector("#slider").style.backgroundColor = "#f06c64";
+      document.querySelector("#slider").style.borderColor = "#ea676b";
+      document.querySelector("#slides2").classList.add("hidden");
+    }, 300);
+  } else {
+    document.querySelector("#slides1").style.animation =
+      "slideRightClose 0.3s linear forwards";
+    document.querySelector("#slides2").style.animation =
+      "slideRightOpen 0.3s linear forwards";
+    setTimeout(function() {
+      document.querySelector("#slides1").classList.add("hidden");
+      document.querySelector("#slider").style.backgroundColor = "#648BF0";
+      document.querySelector("#slider").style.borderColor = "#648BF0";
+      document.querySelector("#slides2").classList.remove("hidden");
+    }, 300);
+  }
+});
+
+var getWidth = function(elem) {
+  var width = elem.scrollWidth + "px"; // Get it's height
+  return width;
+};
+
+HORIZONTAL_PHONE.addEventListener("click", () => {
+  if (
+    document.querySelector("#horizontal-black").classList.contains("hidden")
+  ) {
+    document.querySelector("#horizontal-black").classList.remove("hidden");
+  } else {
+    document.querySelector("#horizontal-black").classList.add("hidden");
+  }
+});
+
+VERTICAL_PHONE.addEventListener("click", () => {
+  if (document.querySelector("#vertical-black").classList.contains("hidden")) {
+    document.querySelector("#vertical-black").classList.remove("hidden");
+  } else {
+    document.querySelector("#vertical-black").classList.add("hidden");
+  }
+});
+
+PORTFOLIO.addEventListener("click", event => {
+  PORTFOLIO.querySelectorAll("li > button").forEach(el =>
+    el.classList.remove("active")
+  );
+  var arr =[];
+  CONTAINER.querySelectorAll(".container > img").forEach(el =>
+    arr.push(getComputedStyle(el).order)
+  );
+  shuffle(arr);
+  CONTAINER.querySelectorAll(".container > img").forEach((el, index) =>
+    el.style.order = arr[index]
+  );
+  console.log(arr);
+  event.target.classList.add("active");
+});
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+CONTAINER.addEventListener("click", event => {
+  CONTAINER.querySelectorAll(".container > img").forEach(el =>
+    el.classList.remove("active")
+  );
+  event.target.classList.add("active");
+});
+
+function submitForm() {
+  MESSAGE_BLOCK.classList.remove("hidden");
+  const SUBJECT = document
+    .querySelector("#quote > div > form > input:nth-child(3)")
+    .value.toString();
+  const DESCRIPTION = document
+    .querySelector("#quote > div > form > textarea")
+    .value.toString();
+  const TEMA = document.getElementById("topic");
+  const OPISANIE = document.getElementById("description");
+  SUBJECT != ""
+    ? (TEMA.innerText = `Тема: ${SUBJECT}`)
+    : (TEMA.innerText = "Без темы");
+  DESCRIPTION != ""
+    ? (OPISANIE.innerText = `Описание: ${DESCRIPTION}`)
+    : (OPISANIE.innerText = "Без описания");
+}
+
+CLOSE_BUTTON.addEventListener("click", () => {
+  document.getElementById("topic").innerText = "";
+  document.getElementById("description").innerText = "";
+  MESSAGE_BLOCK.classList.add("hidden");
+  FORM.reset();
+});
